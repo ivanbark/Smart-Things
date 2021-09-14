fb653c146da6989bb84d6dcf824a606a4f29c590
Ivan <gm.ivanbark@gmail.com>
2021-09-14 12:54:05 +0200

    compass added

--- Smart Glasses/DataStoring.py
+++ Smart Glasses/DataStoring.py
@@ -20,7 +20,7 @@ def data_store():
     cur = mariadb_connection.cursor()
 
     # creating table
-    stmt = 'CREATE TABLE IF NOT EXISTS image_data (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, time_date VARCHAR(255), image_path VARCHAR(255), temp DOUBLE);'
+    stmt = 'CREATE TABLE IF NOT EXISTS image_data (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, time_date VARCHAR(255), image_path VARCHAR(255), temp DOUBLE, facing VARCHAR(255));'
     cur.execute(stmt)
 
     # get root file path of every file in given directory
@@ -29,8 +29,18 @@ def data_store():
             image_path = os.path.join(root, file)
             print(image_path)
 
-            # get temp ????
+            # get temp and compass
             temp = round(sh.get_temperature(), 1)
+
+            heading = round(sh.get_compass(), 1)
+            if heading < 45 or heading > 315:
+                facing = "North"
+            elif heading < 135:
+                facing = "East"
+            elif heading < 225:
+                facing = "South"
+            else:
+                facing = "West"
 
             # unixtimestamp of image file
             image_timestamp = os.path.getmtime(image_path)
@@ -38,8 +48,8 @@ def data_store():
             time = image_datetime.strftime("%H:%M:%S %d/%m/%Y")
 
             # inserting data into database table
-            stmt = 'INSERT INTO `image_data` (time_date, image_path, temp) VALUES (%s, %s, %s);'
-            val = (time, image_path, temp)
+            stmt = 'INSERT INTO `image_data` (time_date, image_path, temp, facing) VALUES (%s, %s, %s, %s);'
+            val = (time, image_path, temp, facing)
             cur.execute(stmt, val)
 
     # commit and close
